f654977a12d13fba257ec6a76d5b6ea61012b62c
Pál Földesi <pal-foldesi@users.noreply.github.com>
2021-02-08 06:23:10 +0100

    Moved from Babel to Webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './src/bootstrap.js',
+    devtool: 'source-map',
+    context: __dirname,
+    plugins: [
+        new CopyWebpackPlugin(
+            {
+                patterns: [
+                    {
+                        from: './src/style.css',
+                        to: './style.css'
+                    },
+                    {
+                        from: './src/index.html',
+                        to: './index.html'
+                    }
+                ]
+            }
+        )
+    ],
+    optimization: {
+        minimize: true
+    }
+}
